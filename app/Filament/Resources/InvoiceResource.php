144c2a20f9fca70076617380eeecf067c43cdc5d
Sok Kimsoeurn <sokkimsoeurn@gmail.com>
2022-07-12 12:56:28 +0700

    Add invoices

---
+++ app/Filament/Resources/InvoiceResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InvoiceResource\Pages;
+use App\Filament\Resources\InvoiceResource\RelationManagers;
+use App\Models\Customer;
+use App\Models\Invoice;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InvoiceResource extends Resource
+{
+    protected static ?string $model = Invoice::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('customer_id')
+                    ->relationship('customer', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('invoice_number')
+                    ->required()
+                    ->unique(ignorable: fn (?Model $record): ?Model => $record)
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('bill_date')->format('d/m/Y'),
+                Forms\Components\DatePicker::make('due_date'),
+                Forms\Components\DatePicker::make('expired_date'),
+                Forms\Components\TextInput::make('total')->disabled(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('customer_id'),
+                Tables\Columns\TextColumn::make('invoice_number'),
+                Tables\Columns\TextColumn::make('bill_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('due_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('expired_date')
+                    ->date(),
+                Tables\Columns\TextColumn::make('total'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInvoices::route('/'),
+            'create' => Pages\CreateInvoice::route('/create'),
+            'view' => Pages\ViewInvoice::route('/{record}'),
+            'edit' => Pages\EditInvoice::route('/{record}/edit'),
+        ];
+    }
+}
